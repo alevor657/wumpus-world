a03e59ce1f744fea34fd190fd9ac5f76edab9a66
uttejh <uttejh@gmail.com>
2018-11-11 15:20:30 +0100

    v2

---
+++ src/wumpusworld/MyAgent.java
@@ -0,0 +1,274 @@
+package wumpusworld;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Arrays.asList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Contains starting code for creating your own Wumpus World agent.
+ * Currently the agent only make a random decision each turn.
+ * 
+ * @author Johan Hagelbäck
+ */
+public class MyAgent implements Agent
+{
+    private World w;
+    private int count=-1;
+    private int[] looper = new int[3];
+//    looper[0] = (int) 0;
+    boolean loop1=false;
+    boolean loop2=false;
+    private int grandma= -1;
+    private int mom = -2;
+    private int child = -3;
+    int rnd;
+    ArrayList<ArrayList<Double>> qtable = null;
+    
+    /**
+     * Creates a new instance of your solver agent.
+     * 
+     * @param world Current world state 
+     */
+    public MyAgent(World world)
+    {
+        w = world;   
+    }
+   
+            
+    /**
+     * Asks your solver agent to execute an action.
+     */
+    public void doAction()
+    {
+        if (this.qtable == null) {
+            this.qtable = MyAgent.readTable();
+        }
+
+        this.doTurn();
+        
+//        try {
+//            this.train();
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(MyAgent.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+    
+    public void doTurn() {
+        if (this.w.hasGlitter(this.w.getPlayerX(), this.w.getPlayerY())) {
+            this.w.doAction(this.w.A_GRAB);
+        }else{
+            if (this.w.hasPit(this.w.getPlayerX(), this.w.getPlayerY())) {
+                this.w.doAction(this.w.A_CLIMB);
+            }
+            ArrayList<ArrayList<Double>> qtable = this.qtable;
+            int x = this.w.getPlayerX();
+            int y = this.w.getPlayerY();
+            int stateIndex = Q.getStateIndex(this.w, x, y, this.w.getDirection());
+            double max = 0.0;
+            
+            grandma = mom;
+            mom = child;
+            child = 4*(x-1) + y;
+            
+            if(loop1){
+                if(grandma == child){
+                    loop2 = true;
+                }
+            }
+
+            if(grandma == child){
+                loop1 = true;
+            }
+            
+            
+
+//            System.out.println("loop:"+grandma+","+mom+","+child);
+//            System.out.println("loop1:"+loop1);
+//            System.out.println("loop2:"+loop2);
+            
+            int bestDirection = -1;
+            int prevDirection = -1;
+            int[] directs = {0, 1, 2, 3};
+            int tempStateIndex = -1;
+            int prevMaxState = -1;
+            double prevMax = 0.0;
+            double maxVal=-10000.0;
+            double maxy = -10000.0;
+            
+//            double[] ds = {-2,-2,-2,-2};
+            List<Double> maxys = new ArrayList<>();
+            List<Double> maxys_dup = new ArrayList<>();
+            List<Integer> stateIndices = new ArrayList<>();
+            List<Integer> ds = new ArrayList<>();
+            for (int direct : directs) {
+                if(direct==0 && !this.w.isValidPosition(x, y+1)){
+                    continue;
+                }else if(direct==1 && !this.w.isValidPosition(x+1, y)){
+                    continue;
+                }
+                else if(direct==2 && !this.w.isValidPosition(x, y-1)){
+                    continue;
+                }
+                else if(direct==3 && !this.w.isValidPosition(x-1, y)){
+                    continue;
+                }
+                tempStateIndex = Q.getStateIndex(this.w, x, y, direct);
+                if(!this.w.hasStench(x, y)){
+                    maxVal = qtable.get(tempStateIndex).get(0);
+                }else{
+                    maxVal = Collections.max(qtable.get(tempStateIndex)); 
+                }
+                maxys.add(maxVal);
+                stateIndices.add(tempStateIndex);
+                ds.add(direct);
+//                ds[direct] = maxVal;
+                System.out.println("Direction:"+direct+":"+qtable.get(tempStateIndex));
+                if (maxVal > maxy) {
+                    prevMaxState = stateIndex;
+//                    prevDirection = bestDirection;
+//                    prevMax = maxy;
+                    stateIndex = tempStateIndex;
+                    maxy = maxVal;
+                    bestDirection = direct;
+//                    if(prevDirection==-1){
+//                        prevDirection = bestDirection;
+//                    }
+                }
+            }
+            int direction = bestDirection;
+            System.out.println(direction);
+            int action = qtable.get(stateIndex).indexOf(maxy);
+            
+//            if(loop1 && loop2){
+////                action = qtable.get(prevMaxState).indexOf(prevMax);
+////                direction = Arrays.asList(ds).indexOf(prevMax);
+//                maxys_dup = maxys;
+//                
+//                Collections.sort(maxys);
+//                Collections.reverse(maxys);
+//                System.out.println("maxys:"+maxys);
+//                System.out.println("maxys_dup:"+maxys_dup);
+//                direction = ds.get(maxys_dup.indexOf(maxys.get(1)));              
+//           
+//                int si = stateIndices.get(maxys_dup.indexOf(maxys.get(1)));
+//                
+//                System.out.println(qtable.get(si));
+//                System.out.println(maxys.get(1));
+//
+//                action = qtable.get(si).indexOf(maxys.get(1));
+//                loop1=false;
+//                loop2=false;
+//                System.out.println("looped");
+//                System.out.println("direction:"+direction);
+//                System.out.println("action:"+action);
+//            }
+            if(!this.w.hasStench(x, y)){
+                action=0;
+            }
+    //        int action = qtable.get(stateIndex).indexOf(max);
+
+    //        this.printTable(qtable);
+            System.out.println(qtable.get(stateIndex));
+
+            switch (direction) {
+                case 0:
+                    System.out.println("GOING up");
+                    this.w.turnUp();
+    //                this.w.moveForward();
+                    break;
+                case 1:
+                    System.out.println("GOING RIGHT");
+                    this.w.turnRight();
+    //                this.w.moveForward();
+                    break;
+                case 2:
+                    System.out.println("GOING down");
+                    this.w.turnDown();
+    //                this.w.moveForward();
+                    break;
+                case 3:
+                    System.out.println("GOING Left");
+                    this.w.turnLeft();
+    //                this.w.moveForward();
+                    break;
+            }
+            if(!this.w.hasArrow()){
+                action=0;
+            }
+            switch (action) {
+                case 0:
+                    System.out.println("moving");
+    //                this.w.turnDown();
+                    this.w.moveForward();
+                    break;
+                case 1:
+                    System.out.println("shooting");
+    //                this.w.turnRight();
+                    this.w.shootForward();
+                    break;
+            }
+
+            System.out.println(max);
+        
+        }
+    }
+    
+    private void train() throws InterruptedException {
+    Q q = new Q(20000, 2, 20000, 0.001, this.w.cloneWorld());
+        try {
+            q.train();
+        } catch (IOException ex) {
+            Logger.getLogger(MyAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Trained!");
+    }
+    
+    public static ArrayList<ArrayList<Double>> readTable() {
+        ObjectInputStream objectinputstream = null;
+        
+        ArrayList<ArrayList<Double>> qTable = null;
+        
+//        int mapNr = Integer.parseInt(GUI.getSelectedLevel());
+        
+        try {
+            String path = System.getProperty("user.dir") + System.getProperty("file.separator") + "QTables";
+//            File in = new File(path, "qtable" + mapNr + ".ser");
+            File in = new File(path, "qtable.ser");
+            FileInputStream streamIn = new FileInputStream(in);
+            objectinputstream = new ObjectInputStream(streamIn);
+            qTable = (ArrayList<ArrayList<Double>>) objectinputstream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(objectinputstream != null){
+                try {
+                    objectinputstream .close();
+                } catch (IOException ex) {
+                    Logger.getLogger(MyAgent.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } 
+        }
+        
+        return qTable;
+    }    
+    
+    public void printTable(ArrayList<ArrayList<Double>> t) {
+        for (int i = 0; i < t.size(); i++) {
+            System.out.print("row: " + i + "    ");
+            for (int j = 0; j < t.get(i).size(); j++) {
+                System.out.print("index: " + j + " ");
+                System.out.print(t.get(i).get(j) + "  ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
---
+++ src/wumpusworld/Q.java
@@ -0,0 +1,505 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wumpusworld;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * this.qTable is being mutated?
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Q {
+    private int actionSize;
+    private int stateSize;
+    ArrayList<ArrayList<Double>> qTable;
+    int epochs;
+    double lerningRate;
+    int maxSteps = 2000;
+    
+    World world;
+    
+    double gamma = 0.95;
+    double epsilon = 1.0;
+    double maxEpsilon = 1;
+    double minEpsilon = 0.01;
+    double decayRate = 0.000005;
+    
+    ArrayList<Integer> rewards;
+    
+    public Q(int stateSize, int actionSize, int epochs, double learningRate, World w) throws InterruptedException {
+        this.actionSize = actionSize;
+        this.stateSize = stateSize;
+        this.epochs = epochs;
+        this.lerningRate = learningRate;
+        this.world = w;
+        
+        this.qTable = new ArrayList<ArrayList<Double>>();        
+        this.rewards = new ArrayList<>();
+        
+        
+        String path = System.getProperty("user.dir") + System.getProperty("file.separator") + "QTables";
+        File file = new File(path + System.getProperty("file.separator"), "qtable.ser");
+
+        if (file.exists()) {
+            System.out.println("Reading existing table");
+            Thread.sleep(1000);
+            this.qTable = MyAgent.readTable();
+        } else {
+            System.out.println("Creating new qTable");
+            Thread.sleep(1000);
+            initTable();
+        }
+    }
+    
+    private void initTable() {
+        for (int i = 0; i < stateSize; i++) {
+            this.qTable.add(new ArrayList());
+            this.qTable.set(i, new ArrayList(Collections.nCopies(this.actionSize, 0.0)));
+        }
+        
+//        for (int i = 0; i < this.qTable.size(); i++) {
+//            System.out.print("row: " + i + "    ");
+//            for (int j = 0; j < this.qTable.get(i).size(); j++) {
+//                System.out.print(this.qTable.get(i).get(j) + "  ");
+//            }
+//            System.out.print("\n");
+//        }
+    }
+    
+//    public static int getStateIndex(World w) {
+//        int magic;
+//        int x = w.getPlayerX();
+//        int y = w.getPlayerY();
+//        boolean stench = w.hasStench(x, y);
+//        boolean breeze = w.hasBreeze(x, y);
+//        
+//        if (breeze && stench) {
+//            magic = 0;
+//        } else if (stench) {
+//            magic = 1;
+//        } else if (breeze) {
+//            magic = 2;
+//        } else {
+//            magic = 3;
+//        }
+//        
+//        return 4 * (4 * (x - 1) + (y - 1)) + magic;
+//    }
+    public static int getTileValue(World w, int x, int y){
+
+        if(!w.isVisited(x, y)){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    public static int getMagicValue(World w, int x, int y) {
+        boolean stench = false;
+        boolean breeze = false;
+        int magic = -1;
+        
+        stench = w.hasStench(x, y);
+        breeze = w.hasBreeze(x, y);
+            
+        if(!w.isValidPosition(x, y)){
+            magic = 0;
+        }else if(!w.isVisited(x, y)){
+            magic = 1;
+        }else if (breeze && stench) {
+            magic = 2;
+        } else if (stench) {
+            magic = 3;
+        } else if (breeze) {
+            magic = 4;
+        } else {
+           magic = 5;
+        }
+        if(magic!=-1){
+            return magic;
+        }else{
+            System.out.println("Something is wrong in magic value");
+            System.exit(0);
+            return 0;
+        }
+        
+    }        
+
+    
+    public static int getStateIndex(World w, int x, int y, int direction) {        
+        int down = 0;
+        int right = 0;
+        int up = 0;
+        int left = 0;
+        int tile = 0;
+        
+        if(direction == w.DIR_DOWN){
+            down = getMagicValue(w, x, y-2);
+            right = getMagicValue(w, x+1, y-1);
+            up = getMagicValue(w, x, y);
+            left = getMagicValue(w, x-1, y-1);
+            tile = getTileValue(w, x, y-1);
+        }else if(direction == w.DIR_RIGHT){
+            down = getMagicValue(w, x+1, y-1);
+            right = getMagicValue(w, x+2, y);
+            up = getMagicValue(w, x+1, y+1);
+            left = getMagicValue(w, x, y);
+            tile = getTileValue(w, x+1, y);
+        }else if(direction == w.DIR_UP){
+            down = getMagicValue(w, x, y);
+            right = getMagicValue(w, x+1, y+1);
+            up = getMagicValue(w, x, y+2);
+            left = getMagicValue(w, x-1, y+1);
+            tile = getTileValue(w, x, y+1);
+        }else if(direction == w.DIR_LEFT){
+            down = getMagicValue(w, x-1, y-1);
+            right = getMagicValue(w, x, y);
+            up = getMagicValue(w, x-1, y+1);
+            left = getMagicValue(w, x-2, y);
+            tile = getTileValue(w, x-1, y);
+        }
+        int[] arr = {down, right, up, left};
+        
+//        Arrays.sort(arr);
+        
+        return 2*(6*(6*(6*(arr[3]) + arr[2]) + arr[1]) + arr[0]) + tile;
+    }
+    
+    public void train() throws IOException {
+        for (int i = 0; i < this.epochs; i++) {
+            World world = this.world.cloneWorld();
+            int[] looper = new int[3];
+            Arrays.fill(looper, -1);
+            int grandma= -1;
+            int mom = -2;
+            int child = -3;
+            System.out.println("epoch : " + i);    
+            boolean loop1=false;
+            boolean loop2=false;
+            
+            for (int k = 0; k < this.maxSteps; k++) {
+                boolean wumpusRewardTaken = false;
+                int x = world.getPlayerX();
+                int y = world.getPlayerY();
+                
+//                looper[k%3] = 4*(x-1) + y-1;
+//                
+//                if(looper[0]==looper[2]){
+//                    loop1 = true;
+//                }
+////                else{
+////                    loop1=false;
+////                }
+//                if(loop1 && looper[0]==looper[1]){
+//                    loop2 = true;
+//                    System.out.println("looping");  
+//                }
+                grandma = mom;
+                mom = child;
+                child = 4*(x-1) + y;
+
+                if(loop1){
+                    if(grandma == child){
+                        loop2 = true;
+                    }
+                }
+
+                if(grandma == child){
+                    loop1 = true;
+//                    System.out.println("looping");
+                }
+                
+                int stateIndex = this.getStateIndex(world, x, y, world.getDirection());
+                Random r = new Random();
+                double expTradeoff = r.nextDouble();
+                
+                boolean exploreReward1 = false;
+                boolean exploreReward2 = false;
+                
+                int action = -10;
+                int direction = -1;
+                double maxVal=0.0;
+                double maxy = -100000.0;
+                int tempStateIndex;
+                if (expTradeoff > this.epsilon) {
+                    if(loop1 && loop2){
+                        direction = this.returnValidDirection(world);
+                        action = this.getValidRandomAction(world);
+//                        System.out.println("Taking random step");
+                        loop1=false;
+                        loop2=false;
+                        
+                    }else{
+    //                    double actionValue = Collections.max(this.qTable.get(stateIndex));
+    //                    action = this.qTable.get(stateIndex).indexOf(actionValue);
+                        int bestDirection = -1;
+                        int[] directs = {0,1,2,3};
+                        for (int direct: directs){
+                            if(direct==0 && !world.isValidPosition(x, y+1)){
+                                continue;
+                            }else if(direct==1 && !world.isValidPosition(x+1, y)){
+                                continue;
+                            }
+                            else if(direct==2 && !world.isValidPosition(x, y-1)){
+                                continue;
+                            }
+                            else if(direct==3 && !world.isValidPosition(x-1, y)){
+                                continue;
+                            }
+                            tempStateIndex = this.getStateIndex(world, x, y, direct);
+                            maxVal = Collections.max(this.qTable.get(tempStateIndex));
+
+                            if(maxVal > maxy){
+                                stateIndex = tempStateIndex;
+                                maxy = maxVal;
+                                bestDirection = direct;
+                            }
+                        }
+                        direction = bestDirection;
+                        action = this.qTable.get(stateIndex).indexOf(maxy);
+                    }
+                } else {
+                    System.out.println("Exploring");
+                    direction = this.returnValidDirection(world);
+//                    if(world.hasStench(x, y)){
+                        action = this.getValidRandomAction(world);
+//                    }else{
+//                        action = 0;
+//                    }
+                    
+                }
+                
+                World newWorld = world.cloneWorld();
+                int score1 = newWorld.getScore();
+                
+                if(direction == 0){
+                    newWorld.turnUp();
+//                    System.out.println("GOING up");
+                    if(!newWorld.isVisited(x, y+1)){
+                        exploreReward1 = true;
+                    }
+                }else if(direction == 1){
+                    newWorld.turnRight();
+    //                    System.out.println("GOING right");
+                    if(!newWorld.isVisited(x+1, y)){
+                        exploreReward1 = true;
+                    }
+                }else if(direction == 2){
+                    newWorld.turnDown();
+//                    System.out.println("GOING down");
+                    if(!newWorld.isVisited(x, y-1)){
+                        exploreReward1 = true;
+                    }
+                }else if(direction == 3){
+                    newWorld.turnLeft();
+//                    System.out.println("GOING left");
+                    if(!newWorld.isVisited(x-1, y)){
+                        exploreReward1 = true;
+                    }
+                }else{
+                    System.out.println("Direction is wrong!!");
+                    System.exit(0);
+                }
+                
+                switch (action) {
+                    case 0:
+//                        newWorld.turnDown();
+                        newWorld.moveForward();
+                        exploreReward2 = true;
+                        break;
+                    case 1:
+//                        newWorld.turnRight();
+                        newWorld.shootForward();
+                        newWorld.hasArrow = true;
+                        break;
+                    
+                    default:
+                        System.out.println("action != 1 - 7");
+                        System.out.println(action);
+                        System.exit(1);
+                }
+
+                if (newWorld.hasPit(newWorld.getPlayerX(), newWorld.getPlayerY())) {
+                    newWorld.doAction(newWorld.A_CLIMB);
+                    score1 += 500;
+                }
+                
+                
+                int score2 = newWorld.getScore();
+                double reward = score2 - score1;
+                
+                if (!newWorld.wumpusAlive() && !wumpusRewardTaken) {
+                    reward += 800;
+                    wumpusRewardTaken = true;
+                }
+                
+                if (newWorld.hasGlitter(newWorld.getPlayerX(), newWorld.getPlayerY())) {
+                    newWorld.doAction(newWorld.A_GRAB);
+//                    reward += 600;
+                }
+                if(exploreReward1 && exploreReward2){
+                    reward += 200;
+                }
+                exploreReward1 = false;
+                exploreReward2 = false;
+                
+                int newStateIndex=-1;
+                double prevValue = this.qTable.get(stateIndex).get(action);
+                double maxy1 = -100000.0;
+                int[] directs1 = {0,1,2,3};
+                for (int direct: directs1){
+                    // if valid 
+                    tempStateIndex = this.getStateIndex(newWorld, x, y, direct);
+                    maxVal = Collections.max(this.qTable.get(stateIndex));
+                    if(maxVal > maxy1){
+                        newStateIndex = tempStateIndex;
+                        maxy1 = maxVal;
+                        
+                    }
+                }
+//                double max = Collections.max(this.qTable.get(newStateIndex));
+                
+                this.qTable.get(stateIndex).set(action, prevValue + this.lerningRate * (reward + this.gamma * maxy1 - prevValue));
+                
+                world = newWorld;
+                
+                if (newWorld.gameOver()) {
+                    System.out.println("Steps:"+k);
+                    break;
+                }
+            }
+            
+            this.epsilon = this.minEpsilon + (this.maxEpsilon - this.minEpsilon) * Math.exp(-1 * this.decayRate * i);
+        }
+        
+        this.serializeQtable();
+    }
+    
+    private void serializeQtable() throws IOException {
+        ObjectOutputStream oos = null;
+        FileOutputStream fout = null;
+        
+//        int mapNr = Integer.parseInt(GUI.getSelectedLevel());
+
+//        System.out.println("Writing msp nr " + mapNr);
+                        
+        try {
+            String path = System.getProperty("user.dir") + System.getProperty("file.separator") + "QTables";
+            new File(path).mkdir();
+//            File file = new File(path + System.getProperty("file.separator"), "qtable" + mapNr + ".ser");
+            File file = new File(path + System.getProperty("file.separator"), "qtable.ser");
+
+            if (file.exists()) {
+                file.delete();
+            }
+            
+            fout = new FileOutputStream(file, true);
+            oos = new ObjectOutputStream(fout);
+            oos.writeObject(this.qTable);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if(oos != null){
+                oos.close();
+            } 
+        }
+    }
+    
+//    private boolean checkValidAction(int action, World w) {
+//        int pX = w.getPlayerX();
+//        int pY = w.getPlayerY();
+//        
+//        ArrayList<int[]> directions = new ArrayList<>();
+//        int down[] = { pX, pY - 1 };
+//        int right[] = { pX + 1, pY };
+//        int up[] = { pX, pY + 1 };
+//        int left[] = { pX -1, pY };
+//
+//        directions.add(down);
+//        directions.add(right);
+//        directions.add(up);
+//        directions.add(left);
+//        
+//        int xPos = directions.get(action)[0];
+//        int yPos = directions.get(action)[1];
+//
+//        return w.isValidPosition(xPos, yPos);
+//    }
+    private int returnValidDirection(World w) {
+        int pX = w.getPlayerX();
+        int pY = w.getPlayerY();
+        
+        ArrayList<int[]> directions = new ArrayList<>();
+//        int[] validDirections = {};
+        ArrayList validDirections = new ArrayList();
+        int down[] = { pX, pY - 1 };
+        int right[] = { pX + 1, pY };
+        int up[] = { pX, pY + 1 };
+        int left[] = { pX -1, pY };
+
+        directions.add(up);
+        directions.add(right);
+        directions.add(down);      
+        directions.add(left);
+        for(int i=0;i<4;i++){
+            int xPos = directions.get(i)[0];
+            int yPos = directions.get(i)[1];
+            
+            if(w.isValidPosition(xPos, yPos)){
+                validDirections.add(i);
+            }
+        }
+        Random r = new Random();
+        int randomVal = r.nextInt(validDirections.size());
+        int randDir = (int) validDirections.get(randomVal);
+        
+
+        return randDir;
+    }
+    
+    private int getValidRandomAction(World w) {
+        Random r = new Random();
+        int randomAction;
+        int x = w.getPlayerX();
+        int y = w.getPlayerY();
+        
+        boolean stench = w.hasStench(x, y);
+        if(stench){
+            randomAction = r.nextInt(2);
+        }else{
+            randomAction = 0;
+        }
+        return randomAction;
+    }
+    
+    private int getNrPossibleMoves(World w) {
+        int pX = w.getPlayerX();
+        int pY = w.getPlayerY();
+
+        int nrPossibleMoves = 4;
+
+        if (pX == 1 || pX == 4) {
+            nrPossibleMoves --;
+        }
+
+        if (pY == 1 || pY == 4) {
+            nrPossibleMoves --;
+        }
+        
+        return nrPossibleMoves;
+    }
+}
